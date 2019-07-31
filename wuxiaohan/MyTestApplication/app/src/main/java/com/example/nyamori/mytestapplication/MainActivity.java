0068f51f7e4682c4a5c754dd56663147ea551578
nyamori <wxh446764891@163.com>
2019-07-31 17:48:26 +0800

    [mod]启动了摄像头，获取图像

--- wuxiaohan/MyTestApplication/app/src/main/java/com/example/nyamori/mytestapplication/MainActivity.java
+++ wuxiaohan/MyTestApplication/app/src/main/java/com/example/nyamori/mytestapplication/MainActivity.java
@@ -1,13 +1,243 @@
 package com.example.nyamori.mytestapplication;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity";
+    private Button takePicture;
+    private TextureView mTextureView;
+    private TextureView textureViewAfterEdit;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener;
+    private Size mPreviewSize;
+    private CameraManager mCameraManager;
+    private String cameraID;
+    private CameraDevice mCamera;
+    private CameraCaptureSession mSession;
+    private Handler mHandler;
+    private CaptureRequest.Builder mPreviewBuilder;
+    private ImageReader imageReader;
+    private android.graphics.Canvas canvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{"android.permission.CAMERA"}, 1);
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        initSurfaceView();
+
+        takePicture = (Button) findViewById(R.id.take_picture);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        destroyCamera();
+        super.onDestroy();
+    }
+
+    private void takePicture() {
+    }
+
+    private void initSurfaceView() {
+        textureViewAfterEdit=(TextureView)findViewById(R.id.texture_view_after_edit);
+        mTextureView = (TextureView) findViewById(R.id.m_texture_view);
+        mSurfaceTextureListener=new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                initCamera(width,height);
+                Log.d(TAG, "onSurfaceTextureAvailable: 初始化成功");
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        };
+    }
+
+    private void openCamera() {
+        HandlerThread handlerThread = new HandlerThread("My First Camera2");
+        handlerThread.start();
+        mHandler = new Handler(handlerThread.getLooper());
+        try {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }else {
+                mCameraManager.openCamera(cameraID, deviceStateCallback, mHandler);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void destroyCamera() {
+        if(mCamera!=null){
+            mCamera.close();
+            mCamera=null;
+        }
+    }
+
+    private void initCamera(int width, int height) {
+        // TODO: 19-7-31 专门写个size相关的初始化函数
+        mPreviewSize=new Size(width,height);
+        mCameraManager = (CameraManager) getApplicationContext().getSystemService(CAMERA_SERVICE);
+        try{
+            for (String cameraID:mCameraManager.getCameraIdList()){
+                CameraCharacteristics cameraCharacteristics=mCameraManager.getCameraCharacteristics(cameraID);
+                Integer facing=cameraCharacteristics.get(CameraCharacteristics.LENS_FACING);
+                Log.d(TAG, "initCamera: facing="+facing);
+                Log.d(TAG, "initCamera: cameraID="+cameraID);
+                if(facing==null||!facing.equals(CameraCharacteristics.LENS_FACING_FRONT))continue;
+                this.cameraID=facing.toString();
+                //在这里可以获取相机的特性
+                initImageReader();
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initImageReader() {
+        imageReader=ImageReader.newInstance(mPreviewSize.getWidth(),mPreviewSize.getHeight(), ImageFormat.YUV_420_888,5);
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                //获得相机数据YUV420
+                Image mImage = reader.acquireLatestImage();
+                if(mImage == null) {
+                    Log.e(TAG, "onImageAvailable .............mImage == null");
+                    return;
+                }
+                Log.d(TAG, "onImageAvailable: image="+mImage.toString());
+                // TODO: 19-7-31 将获取的数据输出到TextureView
+
+                mImage.close();
+            }
+        },mHandler);
+    }
+
+    private CameraDevice.StateCallback deviceStateCallback=new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            mCamera=camera;
+            try{
+                takePreview();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            if(mCamera!=null){
+                mCamera.close();
+                mCamera=null;
+            }
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.d(TAG, "onError: 相机未开启 error code="+error);
+            Toast.makeText(MainActivity.this,"相机开启失败",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public void takePreview() throws CameraAccessException {
+        SurfaceTexture surfaceTexture=mTextureView.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(),mPreviewSize.getHeight());
+        Surface previewSurface=new Surface(surfaceTexture);
+        mPreviewBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        mPreviewBuilder.addTarget(previewSurface);
+        mPreviewBuilder.addTarget(imageReader.getSurface());
+        mCamera.createCaptureSession(Arrays.asList(previewSurface,imageReader.getSurface()), mSessionPreviewStateCallback, mHandler);
+    }
+
+    private CameraCaptureSession.StateCallback mSessionPreviewStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            if(mCamera==null)return;
+            mSession = session;
+            //配置完毕开始预览
+            try {
+                //自动对焦
+                mPreviewBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                //打开闪光灯
+                mPreviewBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                //无限次的重复获取图像
+                mSession.setRepeatingRequest(mPreviewBuilder.build(), null, mHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+            Toast.makeText(MainActivity.this, "配置失败", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
+
